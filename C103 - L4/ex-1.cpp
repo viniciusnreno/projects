31087cdf045b6e8962964300054f44bd29fe5b2a
Vinicius Reno <mineiro.reno@gmail.com>
2022-07-27 19:40:34 -0300

    binario

--- C103 - L4/ex-1.cpp
+++ C103 - L4/ex-1.cpp
@@ -1,28 +1,50 @@
 #include <iostream>
 using namespace std;
+
+int binario(int vet[], int n, int x){
+    bool achou = false;
+    int b,m,a,pos;
+    b = 0;
+    a = n-1;
+
+    while((b <= a) && (achou == false)){
+        m = (b+a)/2;
+        if(x < vet[m]){
+            a = m - 1;
+        }else if (x > vet[m]){
+            b = m + 1;
+        }else {
+            achou = true;
+            pos = m;
+        }
+    }
+    if(achou){
+        return pos;
+    }else{
+        return 0;
+    }
+
+    
+}
+
+
 int main(){
-    int n, x, pos;
-    int contS = 0;
-    int contN = 0;
+    int n, x, pos;  
     cin>>n;
     int vet[n];
 
     for(int i=0; i<n; i++){
         cin>>vet[i];
     }
+    
     cin>>x;
-    for(int i=0; i<n; i++){
-        if(x == vet[i]){
-            contS++;
-            pos = i+1;
-        }else{
-            contN++;
-        } 
-    }
-        if(contS == 0){
-            cout<<"Nao encontrado"<<endl;
-        }else{
-            cout<<"Posicao"<<pos<<endl;
-        } 
+    
+    pos = binario(vet,n,x);
+    
+    if(pos == 0){
+        cout<<"Nao encontrado"<<endl;
+    }else{
+        cout<<"Posicao"<<pos<<endl;
+    } 
     return 0;
 }
--- react-course/main.js
+++ react-course/main.js
@@ -44,13 +44,6 @@ function VinicinMainContent() {
     )
 }
 
-// Challenge: Create your own custom React component!
-// Call it "MainContent", and have it return a simple
-// h1 element that says "I'm learning React!"
-
-// Afterward, render it below the Navbar (which
-// you can do inside the ReactDOM.render call below)
-
 ReactDOM.render(
     <div>
         <Navbar />
